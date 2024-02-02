01e61c66aa3d5f6ca6a3d5a06c19ac5f061df506
Devanshu Kumar Dev <devanshuknowsitall@gmail.com>
2024-02-02 08:42:14 +0530

    adding repo, exception and services

---
+++ my-todo-list-api/src/main/java/com/example/mytodolistapi/service/ToDoService.java
@@ -0,0 +1,18 @@
+package com.example.mytodolistapi.service;
+
+import com.example.mytodolistapi.model.ToDo;
+
+import java.util.List;
+
+public interface ToDoService {
+
+    ToDo getToDo(long id);
+
+    List<ToDo> getToDos();
+
+    ToDo addToDo(String description);
+
+    void deleteToDo(long id);
+
+    void updateToDo(long id, boolean completed);
+}
